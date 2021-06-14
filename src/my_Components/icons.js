b1ebd960d3bb0c5d1616d7b174160b9a2040fa54
Zim Agu <zimmagu@gmail.com>
2021-06-14 12:33:24 -0700

    Return function for icon functionalities started

--- src/my_Components/icons.js
+++ src/my_Components/icons.js
@@ -8,9 +8,17 @@
 
 import React from 'react'
 
-function Icon_Logic ({stored_List_Items, complete_Task, remove_Task, update_Task}) {
-    return ( 
-        <div className={todo.isComplete ? 'task-row complete' : 'task-row'} key={index}> 
+function Icon_Logic ({stored_Items, complete_Task, update_Task, remove_Task}) {
+    /* 
+        Return function Explanation:
+        The state of the stored items are passed in as a prop from list_Item_Logic.js
+
+        The current Item being evaluated in those stored items is passed in as the first parameter
+        The Index of the current item is the second parameter and is used as the key to help React identify which items have changed, are added, or are removed
+    */
+    return stored_Items.map((current_Item, item_Index) => 
+        //If the current Item is complete, it is added to the class "completed" so its style (CSS) can be changed to that of a completed task
+        <div className={current_Item.isComplete ? 'task_Row completed' : 'task_Row'} key={item_Index}>
 
         </div>
     )
--- src/my_Components/list_Item_Logic.js
+++ src/my_Components/list_Item_Logic.js
@@ -8,15 +8,16 @@
 
 import React, {useState} from 'react'
 import Form from './form'
+import Icon_Logic from './icons';
 
 function ListItemLogic () {
     const [stored_List_Items, set_list_Item] = useState([]);
 
     const handle_Task_Addition = (current_List_Item) => {
 
-        const store_List_Items = [current_List_Item, ...stored_List_Items];  // Keeps track of the current list item as well as the previous list item
+        const all_List_Items = [current_List_Item, ...stored_List_Items];  // Keeps track of the current list item as well as the previous list item
 
-        set_list_Item(store_List_Items); // Pushes the new value of the list item to storage
+        set_list_Item(all_List_Items); // Pushes the new value of the list item to storage
         console.log(current_List_Item, ...stored_List_Items);
     };
 
@@ -24,6 +25,12 @@ function ListItemLogic () {
     return (
         <>
             <Form onSubmit={handle_Task_Addition}/>  
+            <Icon_Logic 
+                stored_Items={stored_List_Items}
+                complete_Task={complete_List_Item}
+                update_Task={update_List_Item}
+                remove_Task={remove_List_Item}
+            />
         </>
     )
 }
